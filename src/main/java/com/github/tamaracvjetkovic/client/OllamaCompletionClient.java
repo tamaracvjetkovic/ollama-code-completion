3ae6eb9b365ae651859898d53b0cf5c07893e7dd
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-04-07 23:07:24 +0200

    [ADD] Ollama Completion Client

---
+++ src/main/java/com/github/tamaracvjetkovic/client/OllamaCompletionClient.java
@@ -0,0 +1,53 @@
+package com.github.tamaracvjetkovic.client;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class OllamaCompletionClient {
+    private static String ollamaCompletionUrl = "http://localhost:11434/api/generate";
+
+    public static String requestCompletion(String prefix) {
+        /*String cached = com.github.tamaracvjetkovic.inlinecompletion.OllamaCompletionCache.get(prefix);
+        if (cached != null) {
+            return cached;
+        }*/
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            JSONObject json = new JSONObject();
+            json.put("model", "granite-code:3b");
+            //json.put("prompt", "Based on the given code, which may contain errors, autocomplete the rest of the code WITHOUT any extra information and WITHOUT explanatory comments, even if the function is incorrect or incomplete.\n" + prefix);
+            json.put("prompt", "Continue the following code exactly where it left off. Do NOT explain anything. Do NOT include comments. Only return the code continuation based on the context:\n\n" + prefix);
+            json.put("stream", false);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(ollamaCompletionUrl))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                JSONObject jsonResponse = new JSONObject(response.body());
+                String completion = jsonResponse.getString("response");
+
+                //OllamaCompletionCache.put(prefix, completion);
+
+                return completion;
+
+            } else {
+                 throw new IllegalArgumentException("An unexpected error occurred: " + response.statusCode());
+            }
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("An error occurred: " + e.getMessage(), e);
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/tamaracvjetkovic/inlinecompletion/OllamaCompletionProvider.kt
@@ -0,0 +1,44 @@
+package com.github.tamaracvjetkovic.inlinecompletion
+
+import com.github.tamaracvjetkovic.client.OllamaCompletionClient
+import com.intellij.codeInsight.inline.completion.*
+
+import com.intellij.codeInsight.inline.completion.InlineCompletionEvent
+import com.intellij.codeInsight.inline.completion.InlineCompletionProvider
+import com.intellij.codeInsight.inline.completion.InlineCompletionRequest
+import com.intellij.codeInsight.inline.completion.elements.InlineCompletionElement
+
+import com.intellij.codeInsight.inline.completion.elements.InlineCompletionGrayTextElement
+import kotlinx.coroutines.flow.flowOf
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.TextRange
+
+class OllamaCompletionProvider: InlineCompletionProvider {
+    override val id = InlineCompletionProviderID("OllamaCompletionProvider")
+
+    override suspend fun getSuggestion(request: InlineCompletionRequest): InlineCompletionSuggestion {
+        val prefix = getPrefix(request.editor, request.endOffset)
+        val completion = OllamaCompletionClient.requestCompletion(prefix);
+
+        return InlineCompletionSuggestion.Default(
+            flowOf(InlineCompletionGrayTextElement(completion))
+        )
+    }
+
+    override val insertHandler: InlineCompletionInsertHandler
+        get() = object : InlineCompletionInsertHandler {
+            override fun afterInsertion(
+                environment: InlineCompletionInsertEnvironment,
+                elements: List<InlineCompletionElement>
+            ) {}
+        }
+
+    private fun getPrefix(editor: Editor, offset: Int): String {
+        val document = editor.document
+        return document.getText(TextRange.from(0, offset.coerceAtMost(document.textLength)))
+    }
+
+    override fun isEnabled(event: InlineCompletionEvent): Boolean {
+        return event is InlineCompletionEvent.DocumentChange
+    }
+}
